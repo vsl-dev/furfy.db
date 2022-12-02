f6e4b528d0f72244b27b3ab47d377d824c69f410
vsl-dev <vusalrehimli149@gmail.com>
2022-12-02 19:47:13 +0400

    vsl-dev

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 const Database = require("./lib/index");
-const db = new Database("");
-
-db.on("ready", {
-  message: "Connected to db",
+const db = new Database("./databases/asda.json", {
+  databaseName: "Stripe",
+  autoBackup: false,
 });
 
+db.set("test", {
+  id: 121321,
+  username: "vsl-dev",
+});
 db.add("testVarb", 1);
+console.log(db.get("test"), "GET");
+console.log(db.fetch("test"), "FETCH");
+
+db.backup();
